9a7f3514dec2812df95fa386806f7fae3b16efe4
sasham43 <sashasemail@gmail.com>
2017-12-06 22:01:06 -0600

    getting tokens

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 var Spotify = require('spotify-web-api-node');
+var fs = require('fs');
 
 var scopes = ['user-read-private', 'user-read-email', 'user-library-read', 'user-top-read', 'user-read-recently-played', 'user-read-currently-playing', 'user-modify-playback-state', 'user-read-playback-state'],
     redirectUri = 'http://localhost:3009/callback',
@@ -27,6 +28,22 @@ app.get('/auth', function(req, res, next){
 
 app.get('/callback', function(req, res, next){
     console.log('query', req.query);
+    spotify.authorizationCodeGrant(req.query.code)
+    .then(function(data) {
+      console.log('The token expires in ' + data.body['expires_in']);
+      console.log('The access token is ' + data.body['access_token']);
+      console.log('The refresh token is ' + data.body['refresh_token']);
+      refresh_token = data.body['refresh_token'];
+
+      // Set the access token on the API object to use it in later calls
+      spotify.setAccessToken(data.body['access_token']);
+      spotify.setRefreshToken(data.body['refresh_token']);
+
+      fs.writeFile('token.txt', refresh_token)
+      // getTracks();
+    }, function(err) {
+      console.log('Something went wrong!', err);
+    });
     // get the things
     res.sendFile(__dirname + '/done.html');
 });

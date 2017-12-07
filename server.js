4c713ca68947bd75e6cbed6d8808deeeab3a2365
sasham43 <sashasemail@gmail.com>
2017-12-06 21:55:59 -0600

    getting code

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
+require('dotenv').config();
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
+var Spotify = require('spotify-web-api-node');
+
+var scopes = ['user-read-private', 'user-read-email', 'user-library-read', 'user-top-read', 'user-read-recently-played', 'user-read-currently-playing', 'user-modify-playback-state', 'user-read-playback-state'],
+    redirectUri = 'http://localhost:3009/callback',
+    clientId = process.env.SPOTIFY_ID,
+    clientSecret = process.env.SPOTIFY_SECRET,
+    state = 'coco-downtown';
+
+var spotify = new Spotify({
+    redirectUri: redirectUri,
+    clientId: clientId,
+    clientSecret: clientSecret
+});
+
 app.get('/auth', function(req, res, next){
-    // spotify stuff
-    res.send('stuff')
+    var authorizeURL = spotify.createAuthorizeURL(scopes, state);
+
+    console.log(authorizeURL);
+
+    res.send(authorizeURL);
+});
+
+app.get('/callback', function(req, res, next){
+    console.log('query', req.query);
+    // get the things
+    res.sendFile(__dirname + '/done.html');
 });
 
 app.get('/', function(req, res, next){
